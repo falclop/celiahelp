e229bcc5f4189636384cfb8589b7f507bfd1d0ac
Fabian <falcart91@gmail.com>
2025-04-09 17:34:24 +0200

    más cambios

---
+++ src/main/java/com/celiahelp/CeliahelpApplication.java
@@ -0,0 +1,13 @@
+package com.celiahelp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeliahelpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeliahelpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/celiahelp/CeliahelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celiahelp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeliahelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
